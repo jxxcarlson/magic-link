91b1ebf70b30f7a98d327fdb64ad45743fb4eb46
James Carlson <jxxcarlson@gmail.com>
2024-06-22 11:20:28 +0100

    Refactor (move)

--- src/View/Main.elm
+++ src/View/Main.elm
@@ -2,19 +2,20 @@ module View.Main exposing (view)
 
 import Browser
 import Element exposing (Element)
+import Element.Background
 import Element.Font
 import MagicLink.Types
 import MarkdownThemed
 import Pages.Admin
 import Pages.Home
 import Pages.Notes
-import Pages.Parts
 import Pages.SignIn
 import Pages.TermsOfService
 import Route exposing (Route(..))
 import Theme
 import Types exposing (FrontendModel(..), FrontendMsg, LoadedModel)
 import User
+import View.Color
 
 
 noFocus : Element.FocusStyle
@@ -55,22 +56,63 @@ loadedView : Types.LoadedModel -> Element FrontendMsg
 loadedView model =
     case model.route of
         HomepageRoute ->
-            Pages.Parts.generic model Pages.Home.view
+            generic model Pages.Home.view
 
         TermsOfServiceRoute ->
-            Pages.Parts.generic model Pages.TermsOfService.view
+            generic model Pages.TermsOfService.view
 
         Notes ->
-            Pages.Parts.generic model Pages.Notes.view
+            generic model Pages.Notes.view
 
         SignInRoute ->
-            Pages.Parts.generic
+            generic
                 model
                 (\model_ -> Pages.SignIn.view Types.ChildMsg model_.magicLinkModel |> Element.map Types.AuthFrontendMsg)
 
         AdminRoute ->
             if User.isAdmin model.magicLinkModel.currentUserData then
-                Pages.Parts.generic model Pages.Admin.view
+                generic model Pages.Admin.view
 
             else
-                Pages.Parts.generic model Pages.Home.view
+                generic model Pages.Home.view
+
+
+
+---
+
+
+generic : Types.LoadedModel -> (Types.LoadedModel -> Element Types.FrontendMsg) -> Element Types.FrontendMsg
+generic model view_ =
+    Element.column
+        [ Element.width Element.fill, Element.height Element.fill ]
+        [ Pages.SignIn.headerView model.magicLinkModel model.route { window = model.window, isCompact = True } |> Element.map Types.AuthFrontendMsg
+        , Element.column
+            (Element.padding 20
+                :: Element.scrollbarY
+                :: Element.height (Element.px <| model.window.height - 95)
+                :: Theme.contentAttributes
+            )
+            [ view_ model -- |> Element.map Types.AuthFrontendMsg
+            ]
+        , footer model.route model
+        ]
+
+
+footer : Route -> Types.LoadedModel -> Element msg
+footer route model =
+    Element.el
+        [ Element.Background.color View.Color.blue
+        , Element.paddingXY 24 16
+        , Element.width Element.fill
+        , Element.alignBottom
+        ]
+        (Element.wrappedRow
+            ([ Element.spacing 32
+             , Element.Background.color View.Color.blue
+             , Element.Font.color (Element.rgb 1 1 1)
+             ]
+                ++ Theme.contentAttributes
+            )
+            [ Element.el [ Element.Font.color (Element.rgb 1 1 1) ] (Element.text model.message)
+            ]
+        )
